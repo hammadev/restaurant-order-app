5bd4fa1ef32b552f7a8ff6c4d413d716f405981d
Shariq Shahid <shariqshahid905@gmail.comgit add origin to maingit initgit addgit config --global user.email shariqshahid905@gmail.com>
2022-12-16 23:18:33 +0500

    Checkout UI Fixed

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,84 +1,96 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState } from "react";
 import Header from "./src/components/navbar";
 import InputField from "./src/components/Input";
 import { Container, Row, Col, Button, Text, Card } from "@nextui-org/react";
 import TextArea from "./src/components/TextArea";
 import { GlobalStateContext } from "./contexts/GlobalContext";
-import Cart from './src/components/Cart';
+import Cart from "./src/components/Cart";
 
 const Checkout = () => {
+  const { CartItems } = useContext(GlobalStateContext);
+  console.log(CartItems);
+  let totalPrice = 0;
 
-    const { CartItems } = useContext(GlobalStateContext);
-    console.log(CartItems);
-    let totalPrice = 0;
+  const [deliveryType, SetTypeDeliveryType] = useState();
+  const [paymentType, SetPaymentType] = useState();
 
-    const [deliveryType,SetTypeDeliveryType] = useState();
-    const [paymentType,SetPaymentType] = useState();
+  return (
+    <main>
+      <Container>
+        {/* <Header/> */}
+        <Card css={{ marginTop: 30, overflow: "hidden", padding: 20 }}>
+          <Card.Body>
+            <Row wrap="wrap" gap={1} className="default-row">
+              <Col span={8}>
+                <Text b>Delivery Type</Text>
+                <div style={{ display: "flex", marginTop: 5 }}>
+                  <Button color="primary" auto>
+                    Home Delivery
+                  </Button>
+                  <Button
+                    bordered
+                    color="primary"
+                    css={{ marginLeft: 10 }}
+                    auto
+                  >
+                    Take Away
+                  </Button>
+                </div>
 
-    
-    return (
-        <main>
-            <Container>
-                {/* <Header/> */}
-                <Card css={{ marginTop: 30, overflow: 'hidden', padding: 20 }}>
-                    <Card.Body>
-                        <Row wrap="Wrap">
-                            <Col span={8}>
-                                <Text b>Delivery Type</Text>
-                                <div style={{ display: 'flex', marginTop: 5 }}>
-                                    <Button color="primary" auto>
-                                        Home Delivery
-                                    </Button>
-                                    <Button bordered color="primary" css={{ marginLeft: 10 }} auto>
-                                        Take Away
-                                    </Button>
-                                </div>
-                                <InputField placeHolder="Delivery Address" />
-                                <Row wrap="Wrap" >
-                                    <Col span={6}>
-                                        <InputField placeHolder="Street No." />
-                                    </Col>
-                                    <Col span={6}>
-                                        <InputField placeHolder="Delivery Address" />
-                                    </Col>
-                                </Row>
-                                <TextArea css={{ marginTop: 10, width: '100%' }} />
+                <Row wrap="Wrap" gap={1} className="default-row">
+                  <Col span={12}>
+                    <InputField placeHolder="Delivery Address" />
+                  </Col>
+                  <Col span={6}>
+                    <InputField placeHolder="Street No." />
+                  </Col>
+                  <Col span={6}>
+                    <InputField placeHolder="Delivery Address" />
+                  </Col>
+                  <Col span={12}>
+                    <TextArea css={{ marginTop: 10, width: "100%" }} />
+                  </Col>
+                </Row>
 
-                                <div style={{ marginTop: 20 }}>
-                                    <Text b>Choose Payment Type</Text>
-                                </div>
-                                <div style={{ display: 'flex', marginTop: 5 }}>
-                                    <Button color="primary" auto>
-                                        Cash On Delivery
-                                    </Button>
-                                    <Button bordered color="primary" css={{ marginLeft: 10 }} auto>
-                                        Online Payment
-                                    </Button>
-                                </div>
-                            </Col>
-                            <Col span={4}>
-                                <div className=''>
-                                    <Cart/>
-                                    {/* {
-                                        CartItems.map((item, i) => {
-                                            console.log("totalPrice", item.total_price);
-                                            totalPrice += item.total_price;
-                                            return (
-                                                <div key={i} style={{ marginBottom: 10 }}>
-                                                    <CartItem item={item} />
-                                                </div>
-                                            );
-                                        })
-                                    } */}
-                                </div>
-                            </Col>
-                        </Row>
-                    </Card.Body>
-
-                </Card>
-            </Container>
-        </main>
-    );
-}
+                <div style={{ marginTop: 20 }}>
+                  <Text b>Choose Payment Type</Text>
+                </div>
+                <div style={{ display: "flex", marginTop: 5 }}>
+                  <Button color="primary" auto>
+                    Cash On Delivery
+                  </Button>
+                  <Button
+                    bordered
+                    color="primary"
+                    css={{ marginLeft: 10 }}
+                    auto
+                  >
+                    Online Payment
+                  </Button>
+                </div>
+              </Col>
+              <Col span={4}>
+                <div className="checkout-side-area">
+                  <Cart />
+                  {/* {
+                    CartItems.map((item, i) => {
+                      console.log("totalPrice", item.total_price);
+                      totalPrice += item.total_price;
+                      return (
+                        <div key={i} style={{ marginBottom: 10 }}>
+                          <CartItem item={item} />
+                        </div>
+                      );
+                    })
+                  } */}
+                </div>
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
+      </Container>
+    </main>
+  );
+};
 
 export default Checkout;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,14 @@
 import React, { useEffect, useState } from "react";
-import Header from './src/components/navbar'
-import ProductList from './src/components/ProductList'
-import { Row, Text, Col, Loading, } from "@nextui-org/react";
-import Cart from './src/components/Cart';
-import Menu from './src/components/Menu';
+import Header from "./src/components/navbar";
+import ProductList from "./src/components/ProductList";
+import { Row, Text, Col, Loading } from "@nextui-org/react";
+import Cart from "./src/components/Cart";
+import Menu from "./src/components/Menu";
 import axios from "axios";
 import { BASE_URL } from "./api/config";
-import CartModal from './src/components/Modal';
+import CartModal from "./src/components/Modal";
 
-
-export default function Home()  {
-
+export default function Home() {
   const [data, setData] = useState();
   const [loading, setIsLoading] = useState(true);
 
@@ -18,7 +16,7 @@ export default function Home()  {
     let data = await axios.get(`${BASE_URL}categories`);
     setData(data.data);
     setIsLoading(false);
-  }
+  };
 
   useEffect(() => {
     fetchData();
@@ -26,7 +24,15 @@ export default function Home()  {
 
   if (loading) {
     return (
-      <div style={{ width: '100%', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <div
+        style={{
+          width: "100%",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <Loading size="xl" />
       </div>
     );
@@ -34,22 +40,29 @@ export default function Home()  {
 
   return (
     <main>
-      <div className='Homebanner'>
+      <div className="Homebanner">
         <Header />
       </div>
       <Menu category={data} />
 
-      <div style={{ width: '100%', overflow: 'hidden', position: 'relative' }}>
-        <Row gap={1} wrap='Wrap'>
+      <div style={{ width: "100%", overflow: "hidden", position: "relative" }}>
+        <Row gap={1} wrap="Wrap" className="default-row">
           <Col span={9}>
-            {
-              data.map((item, i) => (
-                <div key={i} id={`cat_sec_${item.id}`}>
-                  <Text h1 style={{ marginTop: 50, marginBottom: 10, textAlign: 'center' }} >{item.name}</Text>
-                  <ProductList categoryId={item.id} />
-                </div>
-              ))
-            }
+            {data.map((item, i) => (
+              <div key={i} id={`cat_sec_${item.id}`}>
+                <Text
+                  h1
+                  style={{
+                    marginTop: 50,
+                    marginBottom: 10,
+                    textAlign: "center",
+                  }}
+                >
+                  {item.name}
+                </Text>
+                <ProductList categoryId={item.id} />
+              </div>
+            ))}
           </Col>
           <Col span={3}>
             <Cart />
@@ -59,6 +72,5 @@ export default function Home()  {
 
       <CartModal />
     </main>
-  )
-  
+  );
 }
